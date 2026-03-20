773d80109a9d426d4224b4a5cb36e7432a8397fa
tovjemam <zbyvaj@seznam.cz>
2026-03-20 21:24:46 +0100

    Add pig_static to random vehicle pool

--- src/game/openworld.cpp
+++ src/game/openworld.cpp
@@ -18,7 +18,7 @@ namespace game
 
 static const char* GetRandomCarModel()
 {
-    const char* vehicles[] = {"pickup_hd", "passat", "twingo", "polskifiat", "cow_static"};
+    const char* vehicles[] = {"pickup_hd", "passat", "twingo", "polskifiat", "cow_static", "pig_static"};
     return vehicles[rand() % (sizeof(vehicles) / sizeof(vehicles[0]))];
 }
 
