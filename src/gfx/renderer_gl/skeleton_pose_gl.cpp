121edf9eac5563bb8340f8b3a495928db3e319df
tovjemam <zbyvaj@seznam.cz>
2026-07-15 00:31:43 +0200

    Fix skeleton poses not working in GL renderer on WebGL

--- src/gfx/renderer_gl/skeleton_pose_gl.cpp
+++ src/gfx/renderer_gl/skeleton_pose_gl.cpp
@@ -1,8 +1,13 @@
 #include "skeleton_pose_gl.hpp"
+#include "shader_defs.hpp"
 
 #include <cassert>
 
-gfx::SkeletonPoseGL::SkeletonPoseGL(size_t num_bones) : num_bones_(num_bones), ubo_(GL_UNIFORM_BUFFER, GL_DYNAMIC_DRAW) {}
+gfx::SkeletonPoseGL::SkeletonPoseGL(size_t num_bones) : num_bones_(num_bones), ubo_(GL_UNIFORM_BUFFER, GL_DYNAMIC_DRAW)
+{
+    std::array<glm::mat4, SD_MAX_BONES> buf;
+    ubo_.SetData(buf.data(), buf.size() * sizeof(buf[0]));
+}
 
 void gfx::SkeletonPoseGL::SetData(std::span<const glm::mat4> data)
 {
